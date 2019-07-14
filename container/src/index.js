22bc4410fbb42e02d8c55ed8ad11ca50077e9135
Phillip Shreves <hi@phillips.work>
2019-07-14 10:46:12 -0500

    Test push to test AWS

--- container/src/index.js
+++ container/src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World! Here is a change!'))
+app.get('/', (req, res) => res.send('Hello World! Here is a change! How about this?'))
 app.get('/test', (req, res) => res.send('This is a routing test'))
 
 app.post('/', (req, res) => res.send('This was a POST test'))
