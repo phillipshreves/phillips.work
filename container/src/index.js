04ef4518ca922eb40edfa6e51efabcdf548873b6
Phillip Shreves <hi@phillips.work>
2019-07-14 10:37:50 -0500

    Updated for AWS deployment

--- container/src/index.js
+++ container/src/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! Here is a change!'))
+app.get('/test', (req, res) => res.send('This is a routing test'))
 
-app.listen(port, 3000);
-console.log(`Running on port ${port}`);
+app.post('/', (req, res) => res.send('This was a POST test'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
